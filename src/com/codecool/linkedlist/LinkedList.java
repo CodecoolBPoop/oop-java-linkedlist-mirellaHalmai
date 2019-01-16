6b3ff2614a3eabe3ffaaa408c8226521ee9c45d4
Halmai Mirella <halmaimirella@gmail.com>
2019-01-16 10:27:31 +0100

    starting node class

---
+++ src/com/codecool/linkedlist/LinkedList.java
@@ -0,0 +1,9 @@
+package com.codecool.linkedlist;
+
+public class LinkedList<E> {
+
+    private LinkedListElement head;
+    private LinkedListElement tail;
+    private int lenght = 0;
+
+}
---
+++ src/com/codecool/linkedlist/LinkedListElement.java
@@ -0,0 +1,13 @@
+package com.codecool.linkedlist;
+
+public class LinkedListElement<T> {
+
+    private LinkedListElement next;
+    private LinkedListElement previous;
+    private boolean isHead;
+    private boolean isTail;
+    private T t;
+
+    public LinkedListElement() {
+    }
+}
